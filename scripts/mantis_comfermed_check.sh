a782a62ecf81d8ae5656c78c0ecc0cfb1756d9e0
An-Death <saenarayoursorrow@gmail.com>
2017-02-25 19:50:56 +0300

    снизил частоту обращения к процу

--- scripts/mantis_comfermed_check.sh
+++ scripts/mantis_comfermed_check.sh
@@ -6,13 +6,9 @@
 
 mantis_comf_tasks () {
 
-  case $1 in
-    start) time_control=true ;;
-    stop) time_control=false ;;
-  esac
-
   while $time_control
   do
+  	sleep 1
     until [[ $(date) != $(date -d 17:00:00) ]] && [[ $(date -d mon) && $(date -d tue) && $(date -d wed) && $(date -d thu) && $(date -d fri) ]]
     do
       tasks_id=$(mysql -h 192.168.0.100 -P 33056 -umantis -pmantis mantis -e "select mbt.id as 'ID' from mantis_bug_table mbt INNER JOIN mantis_category_table mct ON (mbt.category_id=mct.id) LEFT OUTER JOIN mantis_user_table mut ON (mbt.handler_id=mut.id) where mbt.status=40 and mut.username in ('a.simuskov') order by last_updated desc;")
