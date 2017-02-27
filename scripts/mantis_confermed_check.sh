e60d6e33994be10c27adf0ad6bf9f49043a9e434
An-Death <saenarayoursorrow@gmail.com>
2017-02-27 13:33:10 +0300

    +secure data

--- scripts/mantis_confermed_check.sh
+++ scripts/mantis_confermed_check.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #Скрипт для вывода закомвермленных тасков
+
+#Загрузка сурсов с пасами и ip
+source ~/home/as/Документы/scr/secure_scripts/data.cfg
+
 start_man_conf_task=true
 
   while $start_man_conf_task
@@ -8,7 +12,7 @@ start_man_conf_task=true
   	sleep 1
     until [[ $(date) != $(date -d 17:00:00) ]] && [[ $(date -d mon) && $(date -d tue) && $(date -d wed) && $(date -d thu) && $(date -d fri) ]]
     do
-      tasks_id=$(mysql -h 192.168.0.100 -P 33056 -umantis -pmantis mantis -e "select mbt.id as 'ID' from mantis_bug_table mbt INNER JOIN mantis_category_table mct ON (mbt.category_id=mct.id) LEFT OUTER JOIN mantis_user_table mut ON (mbt.handler_id=mut.id) where mbt.status=40 and mut.username in ('a.simuskov') order by last_updated desc;")
+      tasks_id=$(mysql -h $mysmantis -e "select mbt.id as 'ID' from mantis_bug_table mbt INNER JOIN mantis_category_table mct ON (mbt.category_id=mct.id) LEFT OUTER JOIN mantis_user_table mut ON (mbt.handler_id=mut.id) where mbt.status=40 and mut.username in ('a.simuskov') order by last_updated desc;")
       for tasks in $tasks_id 
       do
         if [[ $tasks =~ [0-9]{6} ]]
