f0f076fcec21f24413140b65e8e02af9f7170036
An-Death <saenarayoursorrow@gmail.com>
2017-03-02 09:12:13 +0300

    Поменял время

--- scripts/aulogin_control.sh
+++ scripts/aulogin_control.sh
@@ -3,19 +3,19 @@
 source /home/as/Документы/scr/source/bash_source_color.cfg
 
 
-if ( id as | grep -o nopasswdlogin > /dev/null ) || [[ $(date) > $(date -d 18:40) ]] 
+if ( id as | grep -o nopasswdlogin > /dev/null ) || [[ $(date) > $(date -d 16:49) ]]
 	then
-		if [[ $(date) > $(date -d 18:44) ]]
+		if [[ $(date) > $(date -d 16:49) ]]
 			then
 				echo -e "$BBlue"`date`"$Color_Off" "$BRed"`/usr/sbin/deluser as nopasswdlogin`"$Color_Off" 
 			else
-				echo -e "$BRed""[FAIL] Ошибка выполнения скрипта. Запуск контроля не должен происходить ранее 18:45!""$Color_Off" && exit 1
+				echo -e "$BRed""[FAIL] Ошибка выполнения скрипта. Запуск контроля не должен происходить ранее 17:00!""$Color_Off" && exit 1
 		fi
 	else
-		if [[ $(date) > $(date -d 07:44) ]]
+		if [[ $(date) > $(date -d 07:49) ]]
 			then
 				echo -e "$BBlue"`date`"$Color_Off" "$BGreen"`/usr/sbin/adduser as nopasswdlogin`"$Color_Off"  
 			else
-				echo -e "$BGreen""[FAIL] Ошибка выполнения скрипта. Отмена контроля не должна происходить ранее 07:45!""$Color_Off" && exit 1
+				echo -e "$BGreen""[FAIL] Ошибка выполнения скрипта. Отмена контроля не должна происходить ранее 08:00!""$Color_Off" && exit 1
 			fi
 fi
